529ab6cd982f1bcdd74154efffae1dd37ec05cc6
Michael_SVAO <vierumihail7@gmail.com>
2022-12-06 15:05:04 +0300

    Задача №23

---
+++ dz2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int num = 1;
+Console.WriteLine($"Таблица кубов числа {N}, от 1 до {N}");
+while ( num < N )
+{
+   int result = num * num * num;
+   Console.Write(result + "," );
+   num ++;
+
+}
+Console.Write(N*N*N);
